2536583f6ca339248dfb45ab454f0508fdf8b61f
Arvind <thakur122arvind@gmail.com>
2024-09-30 15:55:37 +0530

    frontend deployment

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-gray-700 h-screen text-white text-6xl">
+     <h1>Hare Krishna</h1>
+    </div>
+  );
+}
+
+export default App;
