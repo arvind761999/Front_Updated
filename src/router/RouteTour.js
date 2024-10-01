48345722b059b343d09696bdf73712a93b34b6a5
Arvind <thakur122arvind@gmail.com>
2024-10-01 15:13:29 +0530

    Add Register, ResetPassword components

--- src/router/RouteTour.js
+++ src/router/RouteTour.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 import Admin from "../pages/Admin";
 import Home from "../pages/Home";
 import { AuthContext } from "../context/authContext";
-
+import ResetPassword from "../pages/ResetPassword";
 const RouteTour = () => {
   const ProtectedRoute = ({ children }) => {
     const { user } = useContext(AuthContext);
@@ -18,9 +19,9 @@ const RouteTour = () => {
 
   return (
     <Routes>
-       <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-
+      <Route path="/reset-password" element={<ResetPassword />} />
       <Route
         path="/admin"
         element={
@@ -29,6 +30,8 @@ const RouteTour = () => {
           </ProtectedRoute>
         }
       />
+      <Route path="/register" element={<Register />} />
+
     </Routes>
   )
 }
