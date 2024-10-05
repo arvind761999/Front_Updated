0f3713b5b5fff920f5af3a3d650912fe7e6849b7
Arvind <thakur122arvind@gmail.com>
2024-10-05 15:00:14 +0530

    remove proxy from package.json

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "${process.env.REACT_APP_API_URL}/api/" || "http://localhost:8080";
+// export const BASE_URL = "${process.env.REACT_APP_API_URL}/api/" || "http://localhost:8080";
+export const BASE_URL = "${process.env.REACT_APP_API_URL}/api/";
