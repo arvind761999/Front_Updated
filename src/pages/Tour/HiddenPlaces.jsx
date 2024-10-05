89d376d0db84444e0ad38b0a76a491a03a0ef4ec
Arvind <thakur122arvind@gmail.com>
2024-10-05 13:32:08 +0530

    Content update

--- src/pages/Tour/HiddenPlaces.jsx
+++ src/pages/Tour/HiddenPlaces.jsx
@@ -43,7 +43,7 @@ const HiddenPlaces = () => {
     // </div>
     <div class="mx-auto max-w-2xl px-4 pb-16 sm:px-6 lg:max-w-7xl lg:px-8 mt-10">
       <p class="uppercase text-5xl">
-        get a brief about Hidden beauties in srilanka
+        get a brief about Hidden beauties in Madhya Pradesh
       </p>
       {/* <!-- first --> */}
       <div class="grid grid-cols-1 md:grid-cols-2 mt-10 gap-5">
